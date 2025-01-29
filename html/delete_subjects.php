a55467d9586e0baad6e748c840c6b43a81f87de4
Mathachai <Mathachai363@gmail.com>
2025-01-29 11:49:07 +0700

    Add files via upload

---
+++ html/delete_subjects.php
@@ -0,0 +1,16 @@
+<?php
+include_once '../con.db.php';
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $sql = "DELETE FROM subjects WHERE id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $id);
+
+    if ($stmt->execute()) {
+        header("Location: /project/api/admin_dashbord/html/manage_subjects.php");
+    } else {
+        echo "เกิดข้อผิดพลาด: " . $stmt->error;
+    }
+}
+?>
